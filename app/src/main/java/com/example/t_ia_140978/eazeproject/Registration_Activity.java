38ec1d39f84d48e18bb6569cc2a5fe14306afd7d
IAO12 <t-ia-140978@senacsp.edu.br>
2018-03-05 11:56:00 -0300

    Initial commit

---
+++ app/src/main/java/com/example/t_ia_140978/eazeproject/Registration_Activity.java
@@ -0,0 +1,13 @@
+package com.example.t_ia_140978.eazeproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registration_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_layout);
+    }
+}
